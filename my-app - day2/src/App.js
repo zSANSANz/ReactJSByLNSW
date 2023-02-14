899615353c7afe65344684512d5cc4bd0b9f8b66
secretcode <sandipermana88@gmail.com>
2023-02-14 09:20:20 +0700

    learn about react routing

---
+++ my-app - day2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
